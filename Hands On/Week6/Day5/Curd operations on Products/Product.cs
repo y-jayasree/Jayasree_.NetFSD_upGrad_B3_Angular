744220c12cd0dc0c52e2f308344bcbfc82a62821
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-03-27 18:52:38 +0530

    Update Product.cs

--- Hands On/Week6/Day5/Curd operations on Products/Product.cs
+++ Hands On/Week6/Day5/Curd operations on Products/Product.cs
@@ -1,1 +1,21 @@
 
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5)]
+        public string Category { get; set; }
+    }
+}
