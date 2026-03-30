be27121b57a2b12fc8f7e8f8b1a984efb6a689bf
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-03-30 18:04:07 +0530

    Add files via upload

---
+++ Hands On/Week6/SOLID principles/SRP/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+            public int Marks { get; set; }
+    }
+
+}
