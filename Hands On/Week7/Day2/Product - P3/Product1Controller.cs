50233d4c255ce61785f6e4acdf45c538c0663802
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-03-31 17:29:36 +0530

    Update Product1Controller.cs

--- Hands On/Week7/Day2/Product - P3/Product1Controller.cs
+++ Hands On/Week7/Day2/Product - P3/Product1Controller.cs
@@ -1,1 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Product1Controller : Controller
+    {
+        private static List<dynamic> products = new List<dynamic>();
+
+        [HttpGet]
+        [Route("product1/index")]
+        public IActionResult Index()
+        {
+            ViewBag.Products = products;
+            return View();
+        }
+        [HttpPost]
+        [Route("product1/index")]
+        public IActionResult Index(string name, double price, int quantity)
+        {
+            // add product to list
+            products.Add(new
+            {
+                Name = name,
+                Price = price,
+                Quantity = quantity
+            });
+
+            ViewBag.Products = products;
+            return View();
+        }
+    }
+}
 
