b2a8def8bfb3d231e18a62b09895a5e32446346a
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-03-31 16:47:46 +0530

    Update StudentController.cs

--- Hands On/Week7/Day2/Student_Registration - P1/StudentController.cs
+++ Hands On/Week7/Day2/Student_Registration - P1/StudentController.cs
@@ -1,1 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        [Route("student/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST 
+        [HttpPost]
+        [Route("student/register")]
+        public IActionResult Register(string name, int age, string course)
+        {
+            // Store data using ViewBag
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Course = course;
+
+            return RedirectToAction("Display", new
+            {
+                name = name,
+                age = age,
+                course = course
+            });
+        }
+
+        [HttpGet]
+        [Route("student/display")]
+        public IActionResult Display(string name, int age, string course)
+        {
+            // receive and store again in viewbag
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Course = course;
+
+            return View();
+        }
+    }
+}
