42296684247b1a0108dffc560bc24df65cc18142
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-03-31 17:13:46 +0530

    Update CalculateController.cs

--- Hands On/Week7/Day2/Calculator - P2/CalculateController.cs
+++ Hands On/Week7/Day2/Calculator - P2/CalculateController.cs
@@ -1,1 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace WebApplication2.Controllers
+{
+    public class CalculateController : Controller
+    {
+        [HttpGet]
+        [Route("calculate/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST 
+        [HttpPost]
+        [Route("calculate/add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            ViewData["Result"] = result;
+
+            return View();
+        }
+    }
+}
