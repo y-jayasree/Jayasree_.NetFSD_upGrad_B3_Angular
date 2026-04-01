e35dbd99f013e29635cd601398ef79f81107efb0
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-01 12:46:46 +0530

    Update FeedbackController.cs
    Added a FeedbackController with GET and POST actions for feedback submission.

--- Hands On/Week7/Day2/Feedback - P4/FeedbackController.cs
+++ Hands On/Week7/Day2/Feedback - P4/FeedbackController.cs
@@ -1,1 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        [HttpGet]
+        [Route("feedback/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [Route("feedback/index")]
+        public IActionResult Index(string name, string comments, int rating)
+        {
+            if (rating >= 4)
+            {
+                ViewData["Message"] = "Thank You for your feedback!";
+            }
+            else
+            {
+                ViewData["Message"] = "We will improve based on your feedback.";
+            }
+
+            return View();
+        }
+    }
+}
 
