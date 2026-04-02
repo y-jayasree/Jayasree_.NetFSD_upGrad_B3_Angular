1bdfcdca64c08ec228b5cfbc7e6b66ba1d606aa7
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-02 14:22:30 +0530

    Add files via upload

---
+++ Hands On/Week7/Day3/Movie/Migrations/20260402065113_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication3.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Rating = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Genre", "Price", "Rating", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Action", 250m, "5", new DateTime(2022, 3, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "A" },
+                    { 2, "Drama", 200m, "4.5", new DateTime(2015, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "B" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
