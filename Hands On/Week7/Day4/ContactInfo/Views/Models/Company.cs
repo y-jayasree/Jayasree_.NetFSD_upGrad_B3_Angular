209c1f2a1379393ca1358f4d20051d00bf129c2b
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-03 14:34:58 +0530

    Add files via upload

---
+++ Hands On/Week7/Day4/ContactInfo/Views/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+
+        // Navigation Property
+        public ICollection<ContactInfo> Contacts { get; set; }
+    }
+}
---
+++ Hands On/Week7/Day4/ContactInfo/Views/Repository/ContactRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication4.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ContactRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return _context.Contacts
+                           .Include(c => c.Company)
+                           .Include(c => c.Department)
+                           .ToList();
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return _context.Contacts
+                           .Include(c => c.Company)
+                           .Include(c => c.Department)
+                           .FirstOrDefault(c => c.ContactId == id);
+        }
+
+        public void AddContact(ContactInfo contact)
+        {
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+        }
+
+        public void UpdateContact(ContactInfo contact)
+        {
+            _context.Contacts.Update(contact);
+            _context.SaveChanges();
+        }
+
+        public void DeleteContact(int id)
+        {
+            var contact = _context.Contacts.Find(id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+                _context.SaveChanges();
+            }
+        }
+
+        // New methods for dropdowns
+        public List<Company> GetAllCompanies()
+        {
+            return _context.Companies.ToList(); // <- never null
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            return _context.Departments.ToList(); 
+        }
+    }
+}
