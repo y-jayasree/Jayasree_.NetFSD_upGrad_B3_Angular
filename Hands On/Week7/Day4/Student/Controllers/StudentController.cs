958c7e2685d0ae845eb35d309361c42a27ea0c02
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-03 18:20:40 +0530

    Add files via upload

---
+++ Hands On/Week7/Day4/Student/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Show Students
+        public IActionResult Index()
+        {
+            var students = _context.Students
+                            .Include(s => s.Course)
+                            .ToList();
+
+            return View(students);
+        }
+
+        // Create std
+        public IActionResult Create()
+        {
+            ViewBag.Courses = new SelectList(_context.Courses, "CourseId", "CourseName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Courses = new SelectList(_context.Courses, "CourseId", "CourseName");
+                return View(student);
+            }
+
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
