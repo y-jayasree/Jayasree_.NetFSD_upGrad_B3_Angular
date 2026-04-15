6db9cc129a4a74676755821cac42a6269da06a13
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-15 14:47:37 +0530

    Add files via upload

---
+++ Hands On/Week9/Day2/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication10.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required,MinLength(3)]
+        public string Name { get; set; }
+        [Required,EmailAddress]
+        public string Email { get; set; }
+    }
+}
