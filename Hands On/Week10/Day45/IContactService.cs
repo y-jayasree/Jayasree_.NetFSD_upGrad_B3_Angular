1923bf9513d01f31516ea4f3deac5e8d66c24552
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-04-21 16:53:44 +0530

    Add files via upload

---
+++ Hands On/Week10/Day45/IContactService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal interface IContactService
+    {
+        // Add new contact
+        void AddContact(Contact contact);
+        // Update existing contact
+        void UpdateContact(Contact contact);
+        //delete contact by id
+        void DeleteContact(int Id);
+        // Get all contacts
+        List<Contact> GetAllContacts();
+    }
+}
